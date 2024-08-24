ef61b86b778f78e3dff91cc40cfdcdeea8100432
Michele Lugano <michele.lugano9@gmail.com>
2024-08-24 20:23:50 +1000

    Hyperparameter search configuration and offloading to clearml agents

--- hyperparameter_search.py
+++ hyperparameter_search.py
@@ -2,14 +2,15 @@ from clearml import Task
 from clearml.automation import HyperParameterOptimizer
 from clearml.automation.optuna import OptunaObjective
 from clearml.automation.parameters import UniformIntegerParameterRange, UniformParameterRange
+from clearml.automation.job import LocalClearmlJob
 from train import train_model
 from constants import get_clearml_project_name, get_params
 
 def objective(params):
     task = Task.init(
         project_name=get_clearml_project_name(),
-        task_name='hyperparameter_optimization',
-        task_type=Task.TaskTypes.optimization,
+        task_name='hyperparameter_optimization_subtask',
+        task_type=Task.TaskTypes.training,
     )
     task.connect(params)
 
@@ -32,19 +33,21 @@ def optimize_hyperparameters():
             UniformIntegerParameterRange('num_layers', min_value=2, max_value=12, step_size=2),
             UniformIntegerParameterRange('d_model', min_value=128, max_value=1024, step_size=128),
             UniformIntegerParameterRange('dff', min_value=512, max_value=4096, step_size=512),
-            UniformParameterRange('dropout_rate', min_value=0.1, max_value=0.5, step_size=0.1),
+            UniformParameterRange('dropout_rate', min_value=0.1, max_value=0.3, step_size=0.1),
         ],
         objective_metric_title='val_accuracy',
         objective_metric_series='val_accuracy',
         objective_metric_sign='max',
-        max_number_of_concurrent_tasks=1,
+        max_number_of_concurrent_tasks=6,
         optimization_time_limit=12 * 60 * 60,
-        compute_time_limit=1 * 60 * 60,
-        total_max_jobs=6,
+        compute_time_limit=2 * 60 * 60,
+        total_max_jobs=10,
         optimizer_class=OptunaObjective,
+        execution_queue='default',  # Queue name for agents to monitor
+        job_class=LocalClearmlJob,
     )
 
-    optimizer.start()
+    optimizer.start_locally()
     optimizer.wait()
     optimizer.stop()
 
@@ -52,4 +55,5 @@ def optimize_hyperparameters():
     print(optimizer.get_best_task())
 
 if __name__ == '__main__':
+    Task.init(project_name=get_clearml_project_name(), task_name='hyperparameter_optimization_controller')
     optimize_hyperparameters()
