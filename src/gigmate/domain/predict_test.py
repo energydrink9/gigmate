Do not compute gradients during scheduled sampling's forward pass. Halved scheduled sampling speed. Fixed `mix_sequences` util function and added tests.

src/gigmate/domain/predict_test.py
@@ -45,10 +45,9 @@ def test_predict_next_note_temperature_zero(mock_model):
         temperature=0,
     )
 
-    assert torch.allclose(result, torch.tensor([1, 0, 1, 1]))
+    assert torch.allclose(result, torch.tensor([1, 0, 1, 1]).reshape((1, 4, 1)))
 
 
-# TODO: fix and re-enable
 @patch('torch.multinomial')
 def test_predict_next_note_with_temperature(mock_multinomial, mock_model):
     mock_multinomial.return_value = torch.tensor([1])

src/gigmate/model/model.py
@@ -220,7 +220,7 @@ class TransformerModel(nn.Module):
         return logits, updated_cache, cross_attention_src
 
 
-def load_ckpt(model, checkpoint_path: str, device: Device) -> None:
+def load_ckpt(model: nn.Module, checkpoint_path: str, device: Device) -> None:
     print(f'Loading ckpt {checkpoint_path}')
     state_dict = torch.load(checkpoint_path, map_location=torch.device(device), weights_only=True)['state_dict']
     

src/gigmate/training/training_model.py
@@ -21,7 +21,7 @@ from gigmate.dataset.dataset import SequenceLengths, get_inputs_and_targets, get
 from gigmate.domain.prediction import complete_sequence
 from gigmate.domain.sampling import sample_from_logits
 from gigmate.model.codec import decode
-from gigmate.model.model import get_model
+from gigmate.model.model import TransformerModel, get_model
 from gigmate.model.utils import compile_model
 from gigmate.training.greedy_lr import GreedyLR
 from gigmate.utils.constants import EPOCHS, MAX_DECODER_SEQ_LEN, get_pad_token_id, get_start_of_sequence_token_id
@@ -40,14 +40,14 @@ CODEBOOKS_LOSS_WEIGHTS: Dict[int, float] = {
     2: 1,
     3: 1,
 }
-CURRICULUM_LEARNING = False
+ENABLE_CURRICULUM_LEARNING = False
 # TODO: Enable compilation of training model
 COMPILE_TRAINING_MODEL = False
 FRAME_RATE = 50
 NUMBER_OF_SAMPLES_FOR_FAD = 5 if ENVIRONMENT != 'dev' else 0
 TEMPERATURE_HIGH = 1.0
 LOG_FRECHET_AUDIO_DISTANCE_IN_TRAINING = False
-SCHEDULED_SAMPLING = True
+ENABLE_SCHEDULED_SAMPLING = True
 SCHEDULED_SAMPLING_TEMPERATURE = 1.0
 
 
@@ -147,7 +147,18 @@ def get_target_audio(target: Tensor, sequence_length: int) -> Tuple[Tensor, int]
 
 
 class TrainingModel(L.LightningModule):
-    def __init__(self, model, learning_rate: float, max_learning_rate: float, min_learning_rate: float, vocab_size: int, codebooks: int, steps_per_epoch: int, resume_epoch: Optional[int], logger: Optional[Logger]):
+    def __init__(
+            self,
+            model: TransformerModel,
+            learning_rate: float,
+            max_learning_rate: float,
+            min_learning_rate: float,
+            vocab_size: int,
+            codebooks: int,
+            steps_per_epoch: int,
+            resume_epoch: Optional[int],
+            logger: Optional[Logger],
+    ):
         super().__init__()
         self.model = model
         self.learning_rate = learning_rate
@@ -240,7 +251,7 @@ class TrainingModel(L.LightningModule):
         metrics: Dict[str, Tensor] = dict()
         total_loss = torch.tensor(0., device=self.device)
 
-        if CURRICULUM_LEARNING:
+        if ENABLE_CURRICULUM_LEARNING:
             # logits have shape (B, V, S) == (B, 2048, 512)
             # max(e^-(x/100), 0.1)
             w = math.e ** -(torch.tensor(range(0, MAX_DECODER_SEQ_LEN), device=self.device.type) / 100)
@@ -303,7 +314,7 @@ class TrainingModel(L.LightningModule):
         return int(self.global_step / self.steps_per_epoch * 4)
 
     def get_scheduled_sampling_probability(self) -> float:
-        return self.trainer.current_epoch / (EPOCHS)
+        return self.trainer.current_epoch / (EPOCHS * 2)
     
     # gold_target: B, K, S, V logits: B, K, S, returns B, K, S
     def get_scheduled_sampling_input(self, gold_target: Tensor, logits: Tensor) -> Tensor:
@@ -316,20 +327,38 @@ class TrainingModel(L.LightningModule):
 
         return mix_sequences(input_sequence, gold_target, scheduled_sampling_probability)
 
-    def schedule_sample(self, gold_target: Tensor, logits: Tensor, full_track: Tensor, sequence_lengths: SequenceLengths):
-        scheduled_sampling_input = self.get_scheduled_sampling_input(gold_target, logits)
+    def schedule_sample(self, stem: Tensor, conditioning_input: Tensor, sequence_lengths: SequenceLengths):
 
-        return self.model(scheduled_sampling_input, conditioning_input=full_track, sequence_lengths=sequence_lengths, shift=get_shift(MAX_DECODER_SEQ_LEN))
+        self.model.eval()
+        # with torch.no_grad():
+        logits, _, _ = self.forward(stem.clone(), conditioning_input.clone(), sequence_lengths)
+        logits = logits.detach()
+        scheduled_sampling_input = self.get_scheduled_sampling_input(stem, logits)
+        self.model.train()
+
+        opt = self.optimizers()
+        if isinstance(opt, List):
+            for optimizer in opt:
+                optimizer.zero_grad()
+        else:
+            opt.zero_grad()
+
+        self.model.zero_grad()
+
+        return self.forward(scheduled_sampling_input, conditioning_input, sequence_lengths)
+    
+    def forward(self, stem: Tensor, conditioning_input: Tensor, sequence_lengths: SequenceLengths):
+        return self.model(stem, conditioning_input=conditioning_input, sequence_lengths=sequence_lengths, shift=get_shift(MAX_DECODER_SEQ_LEN))
 
     def training_step(self, batch: DatasetBatch, batch_idx: int):
         scheduled_sampling_probability = self.get_scheduled_sampling_probability()
         self.log_metric(None, None, 'scheduled_sampling_probability', scheduled_sampling_probability, interval='epoch')
-        
         full_track, stem, targets, sequence_lengths = get_inputs_and_targets(batch, self.device)
-        logits, _, _ = self.model(stem, conditioning_input=full_track, sequence_lengths=sequence_lengths, shift=get_shift(MAX_DECODER_SEQ_LEN))
 
-        if SCHEDULED_SAMPLING is True:
-            logits, _, _ = self.schedule_sample(stem, logits, full_track, sequence_lengths)
+        if ENABLE_SCHEDULED_SAMPLING is True:
+            logits, _, _ = self.schedule_sample(stem, full_track, sequence_lengths)
+        else:
+            logits, _, _ = self.forward(stem, full_track, sequence_lengths)
 
         codebook_logits, codebook_targets, codebook_logits_for_loss = get_codebook_logits_and_targets(self.codebooks, logits, targets)
         loss, loss_metrics, loss_by_codebooks_position = self.compute_loss(codebook_logits_for_loss, codebook_targets, 'train')
@@ -373,15 +402,20 @@ class TrainingModel(L.LightningModule):
         return loss
     
     def predict_continuation(self, full_track: Tensor, length: int, temperature: float):
-        return complete_sequence(self.model, full_track.device.type, full_track, FRAME_RATE, PAD_TOKEN_ID, length / FRAME_RATE, temperature, use_cache=False)
+        self.model.eval()
+        with torch.no_grad():
+            sequence = complete_sequence(self.model, full_track.device.type, full_track, FRAME_RATE, PAD_TOKEN_ID, length / FRAME_RATE, temperature, use_cache=False)
+        self.model.train()
+        return sequence
 
     def validation_step(self, batch: DatasetBatch, batch_idx: int):
 
         full_track, stem, targets, sequence_lengths = get_inputs_and_targets(batch, self.device)
-        logits, _, _ = self.model(stem, conditioning_input=full_track, sequence_lengths=sequence_lengths, shift=get_shift(MAX_DECODER_SEQ_LEN))
 
-        if SCHEDULED_SAMPLING is True:
-            logits, _, _ = self.schedule_sample(stem, logits, full_track, sequence_lengths)
+        if ENABLE_SCHEDULED_SAMPLING is True:
+            logits, _, _ = self.schedule_sample(stem, full_track, sequence_lengths)
+        else:
+            logits, _, _ = self.forward(stem, full_track, sequence_lengths)
 
         codebook_logits, codebook_targets, codebook_logits_for_loss = get_codebook_logits_and_targets(self.codebooks, logits, targets)
         loss, loss_metrics, loss_by_codebooks_position = self.compute_loss(codebook_logits_for_loss, codebook_targets, 'val')
@@ -397,12 +431,9 @@ class TrainingModel(L.LightningModule):
                 full_track_prediction_input = get_full_track_prediction_input(full_track[:1, :, :], sequence_lengths.full_track[0])
                 pred_audio_temp_high_encoded = self.predict_continuation(full_track_prediction_input, length, TEMPERATURE_HIGH)
                 pred_audio_temp_high, pred_audio_temp_high_sr = decode(pred_audio_temp_high_encoded, full_track.device.type, to_cpu=True)
-                # pred_audio_temp_low_encoded = self.predict_continuation(full_track_prediction_input, length, TEMPERATURE_LOW)
-                # pred_audio_temp_low, pred_audio_temp_low_sr = decode(pred_audio_temp_low_encoded, full_track.device.type, to_cpu=True)
                 target_audio, target_audio_sr = get_target_audio(targets_fad.detach(), length)
                 self.log_frechet_audio_distance_metric(batch.size, pred_audio_tf, target_audio, 'val', self.frechet_audio_distance_metric_tf['val'], 'tf')
                 self.log_frechet_audio_distance_metric(batch.size, pred_audio_temp_high, target_audio, 'val', self.frechet_audio_distance_metric_temp_high['val'], 'temp_high')
-                # self.log_frechet_audio_distance_metric(batch.size, pred_audio_temp_low, target_audio, 'val', self.frechet_audio_distance_metric_temp_low['val'], 'temp_low')
 
                 if batch_idx < 8:
                     self.save_generated_audio(batch_idx, pred_audio_tf, pred_audio_temp_high, target_audio, pred_audio_tf_sr, pred_audio_temp_high_sr, target_audio_sr, 'val')

src/gigmate/utils/sequence_utils.py
@@ -228,7 +228,8 @@ def mix_sequences(first_sequence: Tensor, second_sequence: Tensor, probability: 
         raise ValueError("Probability must be between 0 and 1.")
     
     # Create a mask tensor with values drawn from a Bernoulli distribution
-    mask = torch.bernoulli(torch.full(first_sequence.shape, probability, device=first_sequence.device)).to(dtype=torch.int)
+    mask_shape = (first_sequence.shape[0], 1, first_sequence.shape[2])
+    mask = torch.bernoulli(torch.full(mask_shape, probability, device=first_sequence.device)).to(dtype=torch.int)
     
     # Select from the first sequence where mask == 1, and from the second where mask == 0
     mixed_sequence = mask * first_sequence + (1 - mask) * second_sequence

src/gigmate/utils/sequence_utils_test.py
@@ -2,7 +2,7 @@ import pytest
 import torch
 from typing import Tuple
 
-from gigmate.utils.constants import get_pad_token_id, get_special_tokens
+from gigmate.utils.constants import RANDOM_SEED, get_pad_token_id, get_special_tokens
 from gigmate.utils.sequence_utils import apply_interleaving, cut_sequence, get_end_of_sequence_token, get_start_of_sequence_token, pad_sequence
 from gigmate.utils.sequence_utils import remove_special_tokens_from_target_and_logits, revert_interleaving, add_start_and_end_tokens, mix_sequences
 
@@ -182,19 +182,43 @@ def test_add_start_and_end_token(test_sequence):
     assert torch.equal(padded_sequence[:, :, -1:], get_end_of_sequence_token(K, batch_size=B)), "End token is not correct"
 
 
-def test_mix_sequences_prob_0(test_sequence):
+def test_mix_sequences_prob_1(test_sequence):
     sequence, B, K, T = test_sequence
     
-    second_sequence = torch.rand(size=sequence.shape)
+    second_sequence = torch.randint(10, 20, size=sequence.shape)
     mixed_sequence = mix_sequences(sequence, second_sequence, 1.0)
 
     assert torch.equal(mixed_sequence, sequence)
 
 
-def test_mix_sequences_prob_1(test_sequence):
+def test_mix_sequences_prob_0(test_sequence):
     sequence, B, K, T = test_sequence
     
-    second_sequence = torch.rand(size=sequence.shape)
+    second_sequence = torch.randint(10, 20, size=sequence.shape)
     mixed_sequence = mix_sequences(sequence, second_sequence, 0.0)
 
     assert torch.equal(mixed_sequence, second_sequence)
+
+
+def test_mix_sequences_prob_0_5(test_sequence):
+    sequence, B, K, T = test_sequence
+    torch.manual_seed(RANDOM_SEED)
+    second_sequence = torch.randint(10, 20, size=sequence.shape)
+    mixed_sequence = mix_sequences(sequence, second_sequence, 0.5)
+
+    # At a sequence position, the resulting sequence must have codebooks from the same sequence.
+    # We must not mix codebooks from different sequences. 
+
+    expected_sequence = torch.tensor([
+        [[12, 2, 3, 4, 5, 15, 10, 14, 9],
+         [13, 2, 3, 4, 5, 11, 12, 15, 9],
+         [17, 2, 3, 4, 5, 11, 19, 13, 9],
+         [19, 2, 3, 4, 5, 15, 19, 13, 9]],
+
+        [[1, 16, 3, 10, 5, 12, 7, 8, 17],
+         [1, 13, 3, 13, 5, 10, 7, 8, 19],
+         [1, 19, 3, 14, 5, 18, 7, 8, 10],
+         [1, 10, 3, 13, 5, 11, 7, 8, 19]]
+    ])
+
+    assert torch.equal(mixed_sequence, expected_sequence)